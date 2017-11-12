b092052975df798bdc2fe566746542f9c99713cf
Glitch (pebble-harmonica) <none>
2017-11-12 14:56:57 +0000

    Checkpoint

--- whoami.js
+++ whoami.js
@@ -3,10 +3,11 @@
 //'user-agent'
 //'accept-language'
 //req.headers['user-agent']
+//{"ipaddress":"73.182.8.105","language":"en-US","software":"Macintosh; Intel Mac OS X 10_12_6"}
 
 var who = function (data) {
-  console.log(typeof(data.headers['user-agent']));
-  return data.ip;
+  console.log(data.headers['user-agent']);
+  return {"ipaddress":"73.182.8.data.ip;
 }
 
 
